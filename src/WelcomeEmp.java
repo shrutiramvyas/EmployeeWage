90437399c86b1a1f7b69da7b4a65799bd683342c
Shruti <sdnk1993@gmail.com>
2022-08-20 11:32:08 +0530

    Emp_presnt/Absent

--- src/WelcomeEmp.java
+++ src/WelcomeEmp.java
@@ -1,5 +1,11 @@
 public class WelcomeEmp {
     public static void main(String[] args) {
         System.out.println("Welcome Employee Wage");
+        int fullTime =1;
+        int empcheck= (int)(Math.random()*2);
+        if ( empcheck == fullTime )
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Absent");
     }
 }
