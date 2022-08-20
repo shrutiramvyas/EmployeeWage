8d3eea8a3c04a57d2ee04d3a44e5391987632b76
Shruti <sdnk1993@gmail.com>
2022-08-20 13:50:23 +0530

    calculated wages changes

--- src/WelcomeEmp.java
+++ src/WelcomeEmp.java
@@ -1,20 +1,16 @@
 public class WelcomeEmp {
     public static void main(String[] args) {
         int wage_per_hour=20;
-        int no_of_present=1;
-        int total_day=20;
         int working_hour=8;
-        int day=0;
         int total_wage_hour=0;
-        while(day < total_day){
             int isPresent = (int)(Math.random() * 2);
-            if (isPresent == 1 )
-                no_of_present+=1;
-            day++;
-        }
-        int no_of_days=no_of_present*working_hour;
-        total_wage_hour=no_of_days*wage_per_hour;
-        System.out.println("Total wages of employee who is present " + no_of_present +" days out of 20 days is: " + total_wage_hour);
+            if (isPresent == 1){
+                total_wage_hour=working_hour*wage_per_hour;
+                System.out.println("Total wages of employee is: " + total_wage_hour);
+            }
+            else{
+                System.out.println("Total wages of employee is: " + total_wage_hour);
+            }
 
     }
 }
