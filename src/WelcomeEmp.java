13b9cfefbe6ff082615ac57fa96617d432b71822
Shruti <sdnk1993@gmail.com>
2022-08-20 14:16:38 +0530

    using switch case

--- src/WelcomeEmp.java
+++ src/WelcomeEmp.java
@@ -5,16 +5,19 @@ public class WelcomeEmp {
         int total_wage_hour=0;
         int partTime_hour=(working_hour/2);
             int isPresent = (int)(Math.random() * 3);
-            if (isPresent == 1){
-                total_wage_hour=working_hour*wage_per_hour;
-                System.out.println("Total wages of employee who work in full Time Hour is: " + total_wage_hour);
-            }
-            else if(isPresent == 2){
-                total_wage_hour=partTime_hour*wage_per_hour;
-                System.out.println("Total wages of employee who work in part Time Hour: " + total_wage_hour);
-            }
-            else {
-                System.out.println("Total wages of employee is Absent: " + total_wage_hour);
+            switch (isPresent){
+                case 0:
+                    System.out.println("Total wages of employee is Absent " + total_wage_hour);
+                    break;
+                case 1:
+                    total_wage_hour=working_hour*wage_per_hour;
+                    System.out.println("Total wages of employee who work in full Time Hour is: " + total_wage_hour);
+                    break;
+
+                case 2:
+                    total_wage_hour=partTime_hour*wage_per_hour;
+                    System.out.println("Total wages of employee who work in part Time Hour: " + total_wage_hour);
+                    break;
             }
     }
 }
