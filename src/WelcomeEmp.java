259a41f67335a4a3399063761ae222cf60994d9d
Shruti <sdnk1993@gmail.com>
2022-08-20 15:26:36 +0530

    using total days

--- src/WelcomeEmp.java
+++ src/WelcomeEmp.java
@@ -1,23 +1,27 @@
 public class WelcomeEmp {
     public static void main(String[] args) {
-        int wage_per_hour=20;
-        int working_hour=8;
-        int total_wage_hour=0;
-        int partTime_hour=(working_hour/2);
-            int isPresent = (int)(Math.random() * 3);
-            switch (isPresent){
-                case 0:
-                    System.out.println("Total wages of employee is Absent " + total_wage_hour);
-                    break;
-                case 1:
-                    total_wage_hour=working_hour*wage_per_hour;
-                    System.out.println("Total wages of employee who work in full Time Hour is: " + total_wage_hour);
-                    break;
-
-                case 2:
-                    total_wage_hour=partTime_hour*wage_per_hour;
-                    System.out.println("Total wages of employee who work in part Time Hour: " + total_wage_hour);
-                    break;
+                int wage_per_hour=20;
+                int no_of_present=1;
+                int total_day=20;
+                int working_hour=8;
+                int day=0;
+                int total_wage_hour=0;
+                int part_time=(working_hour/2);
+                int no_of_days=1;
+                while(day < total_day){
+                    int isPresent = (int)(Math.random() * 3);
+                    switch (isPresent) {
+                        case 1:
+                            no_of_present+= working_hour;
+                            no_of_days+=1;
+                        case 2:
+                            no_of_present+=part_time;
+                            no_of_days+=0.5;
+                    }
+                    day++;
             }
+        total_wage_hour=no_of_present*wage_per_hour;
+        System.out.println("Total wages of employee who is present " + no_of_days +" days out of 20 days is: " + total_wage_hour);
     }
+
 }
