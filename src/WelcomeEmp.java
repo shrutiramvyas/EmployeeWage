2a849dbdd5052f62f30e5d88c6b911456fcaed95
Shruti <sdnk1993@gmail.com>
2022-08-20 13:58:39 +0530

    Add parttime wages

--- src/WelcomeEmp.java
+++ src/WelcomeEmp.java
@@ -3,14 +3,18 @@ public class WelcomeEmp {
         int wage_per_hour=20;
         int working_hour=8;
         int total_wage_hour=0;
-            int isPresent = (int)(Math.random() * 2);
+        int partTime_hour=(working_hour/2);
+            int isPresent = (int)(Math.random() * 3);
             if (isPresent == 1){
                 total_wage_hour=working_hour*wage_per_hour;
-                System.out.println("Total wages of employee is: " + total_wage_hour);
+                System.out.println("Total wages of employee who work in full Time Hour is: " + total_wage_hour);
             }
-            else{
-                System.out.println("Total wages of employee is: " + total_wage_hour);
+            else if(isPresent == 2){
+                total_wage_hour=partTime_hour*wage_per_hour;
+                System.out.println("Total wages of employee who work in part Time Hour: " + total_wage_hour);
             }
-
+            else {
+                System.out.println("Total wages of employee is Absent: " + total_wage_hour);
+            }
     }
 }
