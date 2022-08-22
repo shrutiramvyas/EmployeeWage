d2a7b30f383df526c4e89e68068b66114a3f8940
Shruti <sdnk1993@gmail.com>
2022-08-22 15:51:36 +0530

    changes

--- src/LengthOfLine.java
+++ src/LengthOfLine.java
@@ -1,6 +1,16 @@
 
+import java.util.Scanner;
+import java.lang.Math;
+
 public class LengthOfLine {
     public static void main(String[] args) {
-        System.out.println("Welcome to calculate length o line");
+        Scanner line = new Scanner(System.in);
+        System.out.println("Enter x1,y1,x2,y2 coordinates");
+        int x1=line.nextInt();
+        int y1=line.nextInt();
+        int x2=line.nextInt();
+        int y2=line.nextInt();
+        double lengthofline = Math.sqrt((x2-x1)*2+(y2-y1)*2);
+        System.out.println("Distance of line is " + lengthofline);
     }
 }
